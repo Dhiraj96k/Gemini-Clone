5d929437e0ae0b90b99e5fe2e5afc2d09ddcca1a
Dhiraj96k <dhirajsalunke7350@gmail.com>
2025-07-05 12:40:04 +0530

    Complete structure of SideBar

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,11 @@
 import React from 'react';
+import Sidebar from './components/Sidebar/Sidebar';
 
 function App() {
     return (
-        <div>
-            
-        </div>
+        <>
+          <Sidebar/>  
+        </>
     );
 }
 
