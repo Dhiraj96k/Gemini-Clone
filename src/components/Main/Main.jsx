45b207aea049004dbc7f6df94a8e272ea8dd46d7
Dhiraj96k <dhirajsalunke7350@gmail.com>
2025-07-05 22:44:26 +0530

    All Design part are done

--- src/components/Main/Main.jsx
+++ src/components/Main/Main.jsx
@@ -1,5 +1,5 @@
 import React from 'react';
-import {assets} from '../../assets/assets.js';
+import { assets } from '../../assets/assets.js';
 import './Main.css';
 
 function Main() {
@@ -8,6 +8,43 @@ function Main() {
             <div className='nav'>
                 <p>Gemini</p>
                 <img src={assets.user_icon} alt="" />
+            </div>
+            <div className="main-container">
+                <div className="greet">
+                    <p><span>Hello, Dev.</span></p>
+                    <p>How Can I Help You Today?</p>
+                </div>
+                <div className="cards">
+                    <div className='card'>
+                        <p>Suggest beautiful places to see on an upcoming road trip</p>
+                        <img src={assets.compass_icon} alt="" />
+                    </div>
+                    <div className='card'>
+                        <p>Briefly summarize this concept: urban planning</p>
+                        <img src={assets.bulb_icon} alt="" />
+                    </div>
+                    <div className='card'>
+                        <p>Brainstrom team bonding activities for our work retreat</p>
+                        <img src={assets.message_icon} alt="" />
+                    </div>
+                    <div className='card'>
+                        <p>Improve the readability of the following code</p>
+                        <img src={assets.code_icon} alt="" />
+                    </div>
+                </div>
+                <div className="main-bottom">
+                    <div className="search-box">
+                        <input type="text" placeholder='Enter a Prompt here' />
+                        <div>
+                            <img src={assets.gallery_icon} alt="" />
+                            <img src={assets.mic_icon} alt="" />
+                            <img src={assets.send_icon} alt="" />
+                        </div>
+                    </div>
+                    <p className="bottom-info">
+                        Gemini may display inaccurate info,including about people,so double-check its responses. Your privacy and Gemini Apps
+                    </p>
+                </div>
             </div>
         </div>
     );
