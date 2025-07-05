a98625669423c43aa4e8a7b29954b1b05bf393e2
Dhiraj96k <dhirajsalunke7350@gmail.com>
2025-07-05 21:46:59 +0530

    Main Content of UI Design

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,14 @@
 import React from 'react';
 import Sidebar from './components/Sidebar/Sidebar';
+import Main from './components/Main/Main';
 
 function App() {
-    return (
-        <>
-          <Sidebar/>  
-        </>
-    );
+  return (
+    <>
+      <Sidebar />
+      <Main />
+    </>
+  );
 }
 
 export default App;
